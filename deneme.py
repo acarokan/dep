962074fe21509087fd2bfa01afbccb871f328696
acarokan <acarokan55@gmail.com>
2016-12-22 08:40:06 +0300

    düzeltme

--- deneme.py
+++ deneme.py
@@ -19,47 +19,74 @@ arama_list = []
 file_list = open(cmake_list , "r")
 parcali = file_list.read().split("find_package")
 parcali.pop(0)
+
+
 for i in parcali:
     deb_liste.append(i.split(")")[0].replace("(",""))
 
+
 for i in  deb_liste:
-        deb_liste_parcali.append(i.split(" "))
+        deb_liste_parcali.append(i.strip("").split(" "))
+print deb_liste_parcali
+
+
+print deb_liste_parcali
     
 for i in deb_liste_parcali:
     for j in i:
         if re.search("\$",j):
             i.pop(i.index(j))
+        elif j == "":
+            i.pop(i.index(j))
     for j in i:
         if re.search("NO_MODULE",j):
+            i.pop(i.index(j))
+        elif j == "":
             i.pop(i.index(j))
     for j in i:
         if re.search("EXACT",j):
             i.pop(i.index(j))
+        elif j == "":
+            i.pop(i.index(j))
     for j in i:
         if re.search("OPTIONAL_COMPONENTS",j):
+            i.pop(i.index(j))
+        elif j == "":
             i.pop(i.index(j))
     for j in i:
         if re.search("NO_POLICY_SCOPE",j):
             i.pop(i.index(j))
-    
+        elif j == "":
+            i.pop(i.index(j))
     for j in i:
         if re.search("QUIET",j):
+            i.pop(i.index(j))
+        elif j == "":
             i.pop(i.index(j))
     for j in i:
         if re.search(".*CONFIG.*",j):
             i.pop(i.index(j))
+        elif j == "":
+            i.pop(i.index(j))
     for j in i:
         if re.search(".*REQUIRED.*",j):
+            i.pop(i.index(j))
+        elif j == "":
             i.pop(i.index(j))
     for j in i:
         if re.search(".*COMPONENTS.*",j):
             i.pop(i.index(j))
+        elif j == "":
+            i.pop(i.index(j))
     for j in i:
         if re.match("[0-9]",j):
             i.pop(i.index(j))
-    for j in i:
-        if j is "":
+        elif j == "":
             i.pop(i.index(j))
+    for j in i:
+        if j == "":
+            i.pop(i.index(j))
+        
     bagimliliklar.append(i)
 
 print "-----Bağımlılıklar listeleniyor-----"
@@ -78,8 +105,7 @@ for i in bagimliliklar:
         ilk = True
 
 
-    
-    
+        
 
 print "-----Bağımlılıkların bulunduğu dizinler listeleniyor-----"
 for i in arama_list:
